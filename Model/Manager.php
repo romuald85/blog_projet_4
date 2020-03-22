c3b3d4afda5ce2d1b171c7d352c10ab46090a9ab
romuald85 <papeboyedk@gmail.com>
2020-03-22 23:45:43 +0100

    refactoring - changement du nom de la base de données

--- Model/Manager.php
+++ Model/Manager.php
@@ -9,7 +9,7 @@ class Manager
   protected function dbConnect()
   {
     $db = new PDO(
-      'mysql:host=localhost;dbname=test;charset=utf8',
+      'mysql:host=localhost;dbname=projet-4_blog;charset=utf8',
       'root',
       '',
       [\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ],
